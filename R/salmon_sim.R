70222dd7ddbf00399d6399b4d1a8618de56e6a44
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2019-07-22 17:45:23 -0700

    Work on function.

--- R/salmon_sim.R
+++ R/salmon_sim.R
@@ -1,23 +1,50 @@
-##' Simulate salmon population dynamics.
+##' @title Simulation of Larkin population dynamics model
 ##'
-##' Simulation Larkin and Ricker simulation models, based on Appendix F of Holt et al. 2018/011 Res Doc... content for \details{} ..  Currently just Larkin, then for Ricker call the Larkin one with parameters set to 0. Or do it's own one.
-##' @title
+##' @description Simulates Larkin population dynamics model, based on Appendix F of Holt
+##'   et al. 2018/011 Res Doc (and what's in our draft manuscript), Ricker
+##'   Appendix, and Grant 2010/042 Res. Doc.
+##' Currently just Ricker, then extend for Larkin.
+##'
 ##' @param a
 ##' @param b
-##' @param p_prime
+##' @param p_prime Vector of typical proportion of recruits spawned in a year that will
+##'   come back to freshwater as age-3, age-4 and age-5 (each of three elements
+##'   in the vector).
 ##' @param omega
 ##' @param sigma_nu
-##' @param sigma_epsilon
-##' @return
-##' @author
+##' @param sigma_epsilon Standard deviation of annual normal deviates on the
+##'     proportions returning at each age.
+##' @param init Vector of five years of initial spawner abundance (units of
+##'   10,000 fish?).
+##' @param T Number of years to run the simulation.
+##' @return Data frame of years (rows) with columns (plus their notation):
+##'   R_t: total recruits returning in year t;
+##'   R_prime_t: number of adult recruits generated from spawners in year t that
+##'     will return to freshwater (and then be subject to fishing and can then
+##'     spawn);
+##'   p_3t, p_4t, p_5t: proportion of R_prime in year t that later return at age 3,
+##'     4 and 5.
 salmon_sim <- function(a = 1,
                        b = 1,
-                       p_prime = c(0.01, 0.98, 0.01)
+                       p_prime = c(0.01, 0.98, 0.01),
                        rho = 0.6,
                        omega = 0.8,
                        sigma_nu = 0.75,
-                       sigma_epsilon = 1
-
+                       sigma_epsilon = 1,
+                       init = c(0.6, 0.01, 0.01, 0.01, 0.6),
+                       T = 100
                        ){
-  R_dash[y] <- S[y] * exp(a * (1 - S[y]) / b  + phi[y]) # fake for now
+
+  init_years <- length(init)       # Initial number of years
+
+  # Generate stochastic variation in p_{g,t}
+  epsilon_gt =
+  p_gt_unnormalized = p_prime * exp(omega * epsilon)
+
+  # Initialize - run for five years to get some randomness
+
+
+  # Loop of full run
+
+  # Return data frame of results
 }
