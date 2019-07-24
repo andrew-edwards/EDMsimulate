50ad91171981e6b978b04d226158ffd0f4ac09b9
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2019-07-23 17:08:19 -0700

    First attempt at full loop.

--- R/salmon_sim.R
+++ R/salmon_sim.R
@@ -69,15 +69,26 @@ salmon_sim <- function(alpha = 0.8,
   R_t <- c(R_t_init, rep(NA, T - length(R_t_init)))
   S_t <- (1 - h_t) * R_t
 
-  R_prime_t <- alpha * S_t * exp(1 - beta[1] * S_t
-                                 - beta[2] * shift(S_t, 1)
-                                 - beta[3] * shift(S_t, 2)
-                                 - beta[4] * shift(S_t, 3)
-                                 + phi_t)        # beta[1] is beta_0
+  R_prime_t <- alpha * S_t * exp(1 -
+                                 beta[1] * S_t -
+                                 beta[2] * shift(S_t, 1) -
+                                 beta[3] * shift(S_t, 2) -
+                                 beta[4] * shift(S_t, 3) +
+                                 phi_t)        # beta[1] is beta_0
 
   # Loop of full run
   for(i in (T_init+1):T){
-    R_prime_t
+    R_prime_t[i] <- alpha * S_t[i] * exp(1 -
+                                         beta[1] * S_t[i] -
+                                         beta[2] * S_t[i-1] -
+                                         beta[3] * S_t[i-2] -
+                                         beta[4] * S_t[i-3] +
+                                         phi_t[i])         # beta[1] is beta_0
+    R_t[i] <- p_tg[i-3] * R_prime_t[i-3] +
+              p_tg[i-4] * R_prime_t[i-4] +
+              p_tg[i-5] * R_prime_t[i-5]
+    S_t[i] <- (1 - h_t[i]) * R_t[i]
+
   }
 
   # Returns matrix frame of results
