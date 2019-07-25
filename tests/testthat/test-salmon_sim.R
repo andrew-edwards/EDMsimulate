38899a126a5a6cd280936c4dcd984171068fa8f1
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2019-07-25 15:25:13 -0700

    Add remaining tests.

--- tests/testthat/test-salmon_sim.R
+++ tests/testthat/test-salmon_sim.R
@@ -16,12 +16,25 @@ test_that("salmon_sim() gives correct answer with original default inputs",{
                        default_sim_seed_42)
 })
 
-test_that("salmon_sim() gives error if length(h_t) is not T",{
+test_that("salmon_sim() gives error if length(h_t) != T",{
           expect_error(salmon_sim(T = 50,
                                   h_t = rep(0.2, 49)))
 })
 
+test_that("salmon_sim() gives error if length(R_t_init) != 8",{
+          expect_error(salmon_sim(R_t_init = 1:7))
+})
+
+test_that("salmon_sim() gives error if sum(p_prime) != 1",{
+          expect_error(salmon_sim(p_prime = c(0.1, 0.1, 0.1)))
+})
+
+test_that("salmon_sim() gives error if all parameters not >= 0",{
+          expect_error(salmon_sim(alpha = -1))
+})
+
 test_that("plot_sim() can create a plot for default run (no seed set)",{
           expect_silent(salmon_run())
+          expect_silent(salmon_run(new_plot=FALSE))
           dev.off()
 })
