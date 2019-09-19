ad0f4cd489b88e15e468bd6fbda086ebdee76069
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2019-09-19 11:24:55 -0700

    Add simple spawner time series to use as example, as have already used it in a lot in explorations.

--- R/data.R
+++ R/data.R
@@ -9,3 +9,20 @@
 #'   using early defaults of the inputs (which are specified in the test).
 #'
 "default_sim_seed_42"
+
+#' Results of simple spawner simulation that shows Issue #15 in rEDM
+#'
+#' @format A vector containing 99 first-differenced values of spawners from a simple
+#' simulation. Using it here as it shows Issue #15 in rEDM.
+#' It is originally from sockeye-sim-edm.rnw that used Carrie's original
+#' function:
+#'  set.seed(42)
+#'  set.seed(42)
+#'  simulated = salmonTraj() # Simulated annual spawner abundances and recruitments
+                             #  (as a list object)
+#'  N = simulated$S             # Just the simulated spawners
+#'  T = length(N)               # Corresponding years
+#'  tvec = 1:T
+#'  x = N[-1] - N[-length(N)]   # x = first-differences
+#'  simple_ts <- x
+"simple_ts"
