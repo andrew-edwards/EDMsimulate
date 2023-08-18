03a670dc56d4df422bb6e6f9c34d3e79ec9bb29e
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2023-08-18 10:23:40 -0700

    Add R_switch to sim_and_fit() so can choose R_t or R_prime_t.

--- R/sim_and_fit.R
+++ R/sim_and_fit.R
@@ -1,7 +1,15 @@
 ##' Simulate a population and fit it using pbsEDM::pbsEDM(), for getting
-##'  full results for any specific simulation. Removes knowledge of the final
-##'  `R_prime_T` since knowing that affects the rho calculations.
+##'  full results for any specific simulation. Allows estimation based on `R_t`
+##'  or `R_prime_t`. Removes knowledge of the final
+##'  value since knowing that affects the rho calculations.
 ##'
+##' @param salmon_sim_args List of arguments to pass onto `salmon_sim()`.
+##' @param pbsEDM_args List of arguments to pass onto `pbsEDM::pbsEDM()`. Note
+##'   that `lags` has no default, so needs to be specified here. First one
+##'   must be called `R_switch` to then get automatically changed to what is
+##'   specified by the `R_switch` parameter
+##' @param R_switch either `R_prime_t` or `R_t` to specify which one
+##'   the calculations are based on.
 ##' @return List object with components:
 ##'  - `simulated:` the simulated tibble
 ##'  - `fit:` the full output from pbsEDM::pbsEDM().
@@ -18,21 +26,22 @@
 ##'                                       first_difference = TRUE))
 ##' res$fit$results$X_rho
 ##' }
-##' @param salmon_sim_args List of arguments to pass onto `salmon_sim()`.
-##' @param pbsEDM_args  List of arguments to pass onto `pbsEDM::pbsEDM()`. Note
-##'   that `lags` has no default, so needs to be specified here.
 sim_and_fit <- function(salmon_sim_args = list(),
                         pbsEDM_args = list(
-                          lags = list(R_prime_t = 0,
-                                      S_t = 0:3))){
-  # want to set seed in a wrapper function to this one, and then include the
-  # epsilton_tg in salmon_sim_args
-  simulated <- do.call(salmon_sim,
-                       salmon_sim_args)   #
+                          lags = list(R_switch = 0,
+                                      S_t = 0:3)),
+                        R_switch = "R_prime_t"){
 
+  stopifnot(R_switch %in% c("R_t", "R_prime_t"))
+  stopifnot(names(pbsEDM_args$lags)[1] == "R_switch")
+  names(pbsEDM_args$lags)[1] <- R_switch
+
+
+  simulated <- do.call(salmon_sim,
+                       salmon_sim_args)
 
   simulated_use <- simulated
-  simulated_use[nrow(simulated_use), "R_prime_t"] = NA    # Ensure no
+  simulated_use[nrow(simulated_use), R_switch] = NA    # Ensure no
                                         # knowledge of it for pbsEDM(), so it
                                         # doesn't affect the rho values (else it does)
 
